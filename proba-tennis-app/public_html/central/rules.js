9fcb2a41ff03dd8bee9459052fe57e899433fc4f
desharnc27 <desharnc27@gmail.com>
2025-02-10 17:40:11 -0500

    core refactor: builtin rules solidified

--- proba-tennis-app/public_html/central/rules.js
+++ proba-tennis-app/public_html/central/rules.js
@@ -1,29 +1,55 @@
+
 export class Rules {
+
     // --- static ---
 
-    static MASTERS = 0;
-    static GRAND_SLAM_2019 = 1;
-    static OLD_WIMBY = 2;
-    static MASTERS_DOUBLE = 3;
-    static FIRST_NEXT_GEN = 4;
-    static OLYMPICS = 5;
-    static OLYMPICS_DOUBLE = 6;
-    static DEBUG = 7;
+    static STANDARD = 0;
+    static GS_2024 = 1;
+    static DOUBLES_2024 = 2;
+    static DOUBLES_GS_2024 = 3;
+    static WIMBY_2021 = 4;
+    static FIRST_NEXT_GEN = 5;
+    static DOUBLES_2009 = 6;
+    static OLD_GS = 7;
+    static OLD_US = 8;
+    static OLD_OLYMPICS = 9;
+    static DEBUG = 10;
 
-    static #NB_FORMATS = 8;
+    static #NB_FORMATS = 11;
 
     static #RULE_NAMES = Rules.#makeRuleNames();
 
+    static #RULE_DETAILS = Rules.#makeRuleDetails();
+
     static #makeRuleNames() {
         const res = new Array(Rules.#NB_FORMATS);
-        res[Rules.MASTERS] = "Masters (default)";
-        res[Rules.GRAND_SLAM_2019] = "Grand Slam 2019";
-        res[Rules.OLD_WIMBY] = "Old Wimbledon";
-        res[Rules.MASTERS_DOUBLE] = "Masters doubles";
-        res[Rules.FIRST_NEXT_GEN] = "First next gen";
-        res[Rules.OLYMPICS] = "Old Olympics";
-        res[Rules.OLYMPICS_DOUBLE] = "Olympics doubles";
-        res[Rules.DEBUG] = "Debug";
+        res[Rules.STANDARD] = "Standard";
+        res[Rules.GS_2024] = "Grand Slam 2024";
+        res[Rules.DOUBLES_2024] = "Doubles 2024";
+        res[Rules.DOUBLES_GS_2024] = "Doubles GS 2024";
+        res[Rules.WIMBY_2021] = "Wimbledon 2021";
+        res[Rules.FIRST_NEXT_GEN] = "proto-next-gen";
+        res[Rules.DOUBLES_2009] = "doubles 2009";
+        res[Rules.OLD_GS] = "Old Grand Slam";
+        res[Rules.OLD_US] = "Old US Open";
+        res[Rules.OLD_OLYMPICS] = "Old Olympics";
+        res[Rules.DEBUG] = "debug format";
+        return res;
+    }
+    
+    static #makeRuleDetails() {
+        const res = new Array(Rules.#NB_FORMATS);
+        res[Rules.STANDARD] = "Standard format for singles";
+        res[Rules.GS_2024] = "Current format for all Grand Slams, men singles";
+        res[Rules.DOUBLES_2024] = "Standard format for doubles";
+        res[Rules.DOUBLES_GS_2024] = "Current format for all Grand Slams, men doubles";
+        res[Rules.WIMBY_2021] = "Short-lived format of Wimbledon, final tiebraker played at 12-12";
+        res[Rules.FIRST_NEXT_GEN] = "Format of the first Next Gen Tournament";
+        res[Rules.DOUBLES_2009] = "Short-lived double format, no advantages";
+        res[Rules.OLD_GS] = "Older Grand Slam format (except USO), doubles & singles, no tiebreaker in final set";
+        res[Rules.OLD_US] = "Older US Open format, doubles & singles, last set is identical to others";
+        res[Rules.OLD_OLYMPICS] = "Older Olympic format, no final tiebreaker";
+        res[Rules.DEBUG] = "Format used for debugging";
         return res;
     }
 
@@ -32,25 +58,39 @@ export class Rules {
     }
 
     static getAllRuleNames() {
-        return [...Rules.#RULE_NAMES];
+        return Rules.#RULE_NAMES.slice();
+    }
+    
+    static getRulesDetail(id) {
+        return Rules.#RULE_DETAILS[id];
+    }
+
+    static getAllRulesDetails() {
+        return Rules.#RULE_DETAILS.slice();
     }
 
     static createRuleById(id) {
         switch (id) {
-            case Rules.MASTERS:
-                return Rules.masters();
-            case Rules.GRAND_SLAM_2019:
-                return Rules.modernGrandSlam();
-            case Rules.OLD_WIMBY:
-                return Rules.oldWimbledon();
-            case Rules.MASTERS_DOUBLE:
-                return Rules.doubleMaster();
+            case Rules.STANDARD:
+                return Rules.defaultRules();
+            case Rules.GS_2024:
+                return Rules.grandSlam2024();
+            case Rules.DOUBLES_2024:
+                return Rules.doubles2024();
+            case Rules.DOUBLES_GS_2024:
+                return Rules.doublesGS2024();
+            case Rules.WIMBY_2021:
+                return Rules.wimby2021();
             case Rules.FIRST_NEXT_GEN:
                 return Rules.firstNextGen();
-            case Rules.OLYMPICS:
-                return Rules.olympics();
-            case Rules.OLYMPICS_DOUBLE:
-                return Rules.doubleOlympic();
+            case Rules.DOUBLES_2009:
+                return Rules.doubles2009();
+            case Rules.OLD_GS:
+                return Rules.oldGS();
+            case Rules.OLD_US:
+                return Rules.oldUSOpen();
+            case Rules.OLD_OLYMPICS:
+                return Rules.oldOlympics();
             default:
                 return Rules.debugSmallCombo();
         }
@@ -59,71 +99,60 @@ export class Rules {
     static defaultRules() {
         return new Rules();
     }
-
-    static masters() {
-        return new Rules();
-    }
-
-    static oldWimbledon() {
-        const pp = new Rules();
-        pp.sizeOfMatch = 3;
-        pp.gapFinalTB = -2;
-        return pp;
-    }
-
-    static modernGrandSlam() {
-        const pp = new Rules();
-        pp.sizeOfMatch = 3;
+    static grandSlam2024() {
+        const pp = Rules.oldUSOpen();
         pp.sizeOfFinalTB = 10;
+        return pp;  
+    }
+    static doubles2024() {
+        const pp = Rules.doublesGS2024();
+        pp.gapFinalTB = -1;
+        pp.sizeOfFinalSet = 1;
+        return pp;  
+    }
+    static doublesGS2024() {
+        const pp = new Rules();
+        pp.sizeOfFinalTB = 10;
+        return pp;  
+    }
+    
+    static wimby2021() {
+        const pp = Rules.oldUSOpen();
+        pp.gapFinalTB = 6;
         return pp;
     }
-
-    static firstNextGen() {
+    static firstNextGen(){
         const pp = new Rules();
         pp.sizeOfMatch = 3;
         pp.sizeOfRegSet = 4;
-        pp.gapRegTB = 0;
+        pp.gapRegTB = -1;
         pp.advantages = false;
         pp.makeFinalSetSameFormat();
         return pp;
     }
-
-    static olympics() {
+    static doubles2009() {
+        const pp = Rules.doubles2024();
+        pp.advantages = false;
+        return pp;
+    }
+    static oldGS() {
+        const pp = Rules.oldUSOpen();
+        pp.gapFinalTB = -2;
+        return pp;
+    }
+    static oldUSOpen() {
+        const pp = new Rules();
+        pp.sizeOfMatch = 3;
+        return pp;
+    }
+    static oldOlympics() {
         const pp = new Rules();
         pp.gapFinalTB = -2;
         return pp;
     }
-
-    static doubleMaster() {
-        const pp = Rules.doubleOlympic();
-        pp.advantages = false;
-        return pp;
-    }
-
-    static doubleOlympic() {
-        const pp = new Rules();
-        pp.sizeOfFinalSet = 1;
-        pp.gapFinalTB = -1;
-        pp.sizeOfFinalTB = 11;
-        return pp;
-    }
-
-    static debugSingleTB() {
-        const pp = Rules.doubleMaster();
-        pp.sizeOfMatch = 1;
-        pp.sizeOfFinalTB = 2;
-        return pp;
-    }
-
-    static debugFakeTB() {
-        const pp = new Rules();
-        pp.sizeOfMatch = 1;
-        pp.advantages = false;
-        pp.sizeOfJeu = 1;
-        pp.gapFinalTB = -2;
-        return pp;
-    }
-
+    
+    
+    
     static debugSmallCombo() {
         const pp = new Rules();
         pp.advantages = false;
@@ -147,8 +176,39 @@ export class Rules {
     }
 
     // --- End of static ---
-    constructor(sizeOfJeu, advantages, sizeOfRegSet, gapRegTB, sizeOfRegTB,
-            sizeOfFinalSet, gapFinalTB, sizeOfFinalTB, sizeOfMatch) {
+    sizeOfJeu;
+    advantages;
+
+    sizeOfRegSet;
+    gapRegTB;
+    sizeOfRegTB;
+
+    sizeOfFinalSet;
+    gapFinalTB;
+    sizeOfFinalTB;
+
+    sizeOfMatch;
+
+    makeFinalSetSameFormat() {
+        this.sizeOfFinalSet = this.sizeOfRegSet;
+        this.gapFinalTB = this.gapRegTB;
+        this.sizeOfFinalTB = this.sizeOfRegTB;
+    }
+
+    /**
+     * Sets the default values for the rules.
+     */
+    setDefault() {
+        this.sizeOfJeu = 4;
+        this.advantages = true;
+        this.sizeOfRegTB = 7;
+        this.sizeOfRegSet = 6;
+        this.gapRegTB = 0;
+        this.makeFinalSetSameFormat();
+        this.sizeOfMatch = 2;
+    }
+
+    constructor(sizeOfJeu, advantages, sizeOfRegSet, gapRegTB, sizeOfRegTB, sizeOfFinalSet, gapFinalTB, sizeOfFinalTB, sizeOfMatch) {
         if (arguments.length === 0) {
             this.setDefault();
         } else {
@@ -164,31 +224,27 @@ export class Rules {
         }
     }
 
-    makeFinalSetSameFormat() {
-        this.sizeOfFinalSet = this.sizeOfRegSet;
-        this.gapFinalTB = this.gapRegTB;
-        this.sizeOfFinalTB = this.sizeOfRegTB;
-    }
-
-    setDefault() {
-        this.sizeOfJeu = 4;
-        this.advantages = true;
-        this.sizeOfRegTB = 7;
-        this.sizeOfRegSet = 6;
-        this.gapRegTB = 0;
-        this.makeFinalSetSameFormat();
-        this.sizeOfMatch = 2;
-    }
-
+    /**
+     * Checks whether the current score is in a tie-break.
+     * @param {number} nj0 - Score of first player.
+     * @param {number} nj1 - Score of second player.
+     * @param {boolean} finalSet - Indicates if it's the final set.
+     * @returns {boolean} True if in tie-break, otherwise false.
+     */
     inTieBreak(nj0, nj1, finalSet) {
         if (nj0 !== nj1 || (finalSet && !this.finalSetHasTieBreak())) {
             return false;
         }
         const tbSeuil = finalSet ? this.sizeOfFinalSet + this.gapFinalTB : this.sizeOfRegSet + this.gapRegTB;
+        //Note: > case should not happen
         return nj0 >= tbSeuil;
     }
 
+    /**
+     * Determines if the final set has a tie-break.
+     * @returns {boolean} True if final set has a tie-break, otherwise false.
+     */
     finalSetHasTieBreak() {
-        return this.sizeOfFinalTB >= 0;
+        return this.gapFinalTB > -2;
     }
 }
